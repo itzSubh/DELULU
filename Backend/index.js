6a4eaf27120b5157691c3b3c637ab3743222bab2
itzSubh <subhash03id@gmail.com>
2026-06-29 23:29:37 +0530

    Initial Commit

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/db.js'
+import dns from 'dns'
+
+dns.setServers([
+    '1.1.1.1',
+    '8.8.8.8'
+])
+
+const app = express();
+
+await connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Server is Running!!!'))
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is runnning on port ${PORT}`))
