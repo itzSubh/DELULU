04ecfc32066df88e823edff9f87266edc9d04af2
itzSubh <subhash03id@gmail.com>
2026-06-30 11:10:06 +0530

    created models

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/db.js'
 import dns from 'dns'
+import { clerkMiddleware } from '@clerk/express'
+
 
 dns.setServers([
     '1.1.1.1',
@@ -15,6 +17,7 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
 app.get('/', (req, res) => res.send('Server is Running!!!'))
 
