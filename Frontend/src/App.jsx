388ddbdc8d6fc1a9e54c52e15e2d094342570b10
itzSubh <subhash03id@gmail.com>
2026-06-30 11:34:56 +0530

    added clerk headers

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from 'react'
-
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 function App() {
 
   return (
-    <div >Heyy</div>
+    <div>
+      <h1>Heyy</h1>
+      <header>
+        <Show when="signed-out">
+          <SignInButton mode='modal' />
+          <SignUpButton mode='modal' />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+    </div>
   )
 }
 
